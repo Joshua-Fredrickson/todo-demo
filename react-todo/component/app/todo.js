46e4a73af6a350aabd73b4b0d083ac26904dec2f
Joshua Fredrickson <uafredrickson@gmail.com>
2018-07-05 17:30:04 -0700

    scaffolding

---
+++ react-todo/component/app/todo.js
@@ -0,0 +1,63 @@
+class MyTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Josh's TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What do I need to do?
+          </label>
+          <input
+          id="new-todo"
+          onChange={this.handleChange}
+          value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length +1}
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(event) {
+    this.setState({ text: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />, mountNode);
